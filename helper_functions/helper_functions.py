f42a40f7a475b8a11b3d7f9724f9743bf75adabc
Kasper Svane <svane.kasper20@gmail.com>
2024-03-18 17:48:43 +0100

    Update helper_functions.py

--- helper_functions/helper_functions.py
+++ helper_functions/helper_functions.py
@@ -266,35 +266,35 @@ def compare_histories(original_history: tf.keras.callbacks.History,
     acc = original_history.history["accuracy"]
     loss = original_history.history["loss"]
 
+    val_acc = original_history.history["val_accuracy"]
+    val_loss = original_history.history["val_loss"]
+
     # Combine original history with new history
     total_acc = acc + new_history.history["accuracy"]
     total_loss = loss + new_history.history["loss"]
 
-    # Make plots
-    plt.figure(figsize=(8, 8))
-    plt.subplot(2, 1, 1)
-    plt.plot(total_acc, label="Training Accuracy")
-    plt.plot(total_loss, label="Training Loss")
-    plt.plot([initial_epochs - 1, initial_epochs - 1],
-             plt.ylim(), label="Start Fine Tuning")  # Re-shift plot around epochs
-    plt.legend(loc="lower right")
-    plt.title("Training Accuracy and Loss")
-
-    # Plot validation
-    plt.subplot(2, 1, 2)
-    val_acc = original_history.history["val_accuracy"]
-    val_loss = original_history.history["val_loss"]
-
     total_val_acc = val_acc + new_history.history["val_accuracy"]
     total_val_loss = val_loss + new_history.history["val_loss"]
 
-    plt.plot(total_val_acc, label="Validation Accuracy")
-    plt.plot(total_val_loss, label="Validation Loss")
-    plt.plot([initial_epochs - 1, initial_epochs - 1],
-             plt.ylim(),
-             label="Start Fine Tuning")  # Re-shift plot around epochs
-    plt.legend(loc="upper right")
-    plt.title("Validation Accuracy and Loss");
+    # Make plots
+    plt.figure(figsize=(8, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(total_acc, label='Training Accuracy')
+    plt.plot(total_val_acc, label='Validation Accuracy')
+    plt.plot([initial_epochs-1, initial_epochs-1],
+              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(total_loss, label='Training Loss')
+    plt.plot(total_val_loss, label='Validation Loss')
+    plt.plot([initial_epochs-1, initial_epochs-1],
+              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    plt.show()
 
 
 # Function to unzip a zipfile into current working directory
